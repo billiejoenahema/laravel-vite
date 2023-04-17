80cdcc5d1fa303028a2d0a49a15a21798266bed4
billiejoenahema <billiejoenahema@gmail.com>
2023-04-18 07:25:26 +0900

    chore: add vite settings to vite.config.js

--- src/vite.config.js
+++ src/vite.config.js
@@ -1,11 +1,24 @@
-import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 import laravel from 'laravel-vite-plugin';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
+  plugins: [
+    vue(),
+    laravel({
+      input: ['resources/css/app.css', 'resources/js/app.js'],
+      refresh: true,
+    }),
+  ],
+  server: {
+    host: true,
+    hmr: {
+      host: 'localhost',
+    },
+  },
+  resolve: {
+    alias: {
+      '@': '/resources/js',
+    },
+  },
 });
