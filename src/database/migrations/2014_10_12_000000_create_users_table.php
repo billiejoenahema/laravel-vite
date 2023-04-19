0bdca3184499a11c0989d5632951bb7aab75881e
billiejoenahema <billiejoenahema@gmail.com>
2023-04-20 07:27:59 +0900

    feat: make create users migration

--- src/database/migrations/2014_10_12_000000_create_users_table.php
+++ src/database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,11 +12,15 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
+            $table->comment('ユーザー');
+
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('name')->comment('ユーザー名');
+            $table->string('email')->unique()->comment('メールアドレス');
+            $table->char('role', 2)->default('00')->comment('ユーザー権限');
+            $table->datetime('last_login_at')->nullable()->comment('最終ログイン日時');
+            $table->timestamp('email_verified_at')->nullable()->comment('メール認証日時');
+            $table->string('password')->comment('パスワード');
             $table->rememberToken();
             $table->timestamps();
         });
