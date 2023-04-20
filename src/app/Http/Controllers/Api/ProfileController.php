f638c22ccb38c1f8b594f78f600e57d3a984883a
billiejoenahema <billiejoenahema@gmail.com>
2023-04-21 07:54:34 +0900

    feat: make profile API

---
+++ src/app/Http/Controllers/Api/ProfileController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\Api\ProfileResource;
+
+class ProfileController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @return ProfileResource
+     */
+    public function __invoke(): ProfileResource
+    {
+        return new ProfileResource(auth()->user());
+    }
+}
--- src/routes/api.php
+++ src/routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => ['auth:sanctum']], function () {
+
+    // ログインユーザー情報
+    Route::get('/profile', ProfileController::class);
 });
