613edaf8ef2eefd17a210da2426746de486a8e1d
billiejoenahema <billiejoenahema@gmail.com>
2023-04-26 09:47:38 +0900

    feat: install fontawesome

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -1,8 +1,17 @@
 import './bootstrap';
 
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import { store } from './store';
 
-createApp(App).use(router).use(store).mount('#app');
+library.add(fas);
+
+createApp(App)
+  .use(router)
+  .use(store)
+  .component('font-awesome-icon', FontAwesomeIcon)
+  .mount('#app');
