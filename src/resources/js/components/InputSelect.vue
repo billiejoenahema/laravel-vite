67233493c62d38864ded813cb52d260c1d68f07d
billiejoenahema <billiejoenahema@gmail.com>
2023-05-13 14:59:39 +0900

    feat: make InputSelect component

---
+++ src/resources/js/components/InputSelect.vue
@@ -0,0 +1,54 @@
+<script setup>
+const props = defineProps({
+  classValue: {
+    type: String,
+    required: false,
+    default: "",
+  },
+  disabled: {
+    default: false,
+    required: false,
+    type: Boolean,
+  },
+  id: {
+    default: "",
+    required: true,
+    type: String,
+  },
+  invalidFeedback: {
+    default: "",
+    required: false,
+    type: String,
+  },
+  options: {
+    default: () => [],
+    required: false,
+    type: Array,
+  },
+  modelValue: {
+    default: "",
+    required: false,
+    type: [String, Number],
+  },
+});
+const emit = defineEmits(["update:modelValue"]);
+const updateModelValue = (event) => {
+  emit("update:modelValue", event.target.value);
+};
+</script>
+
+<template>
+  <select
+    class="form-select"
+    :class="classValue"
+    :disabled="disabled"
+    :id="id"
+    :value="modelValue"
+    @change="updateModelValue"
+  >
+    <option v-for="option in options" :value="option.id">
+      {{ option.name }}
+    </option>
+  </select>
+  <div class="invalid-feedback">{{ invalidFeedback }}</div>
+</template>
