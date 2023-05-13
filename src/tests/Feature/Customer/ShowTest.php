a8b741e4f3793df270b3ca917a88826fde9d9fdc
billiejoenahema <billiejoenahema@gmail.com>
2023-05-13 23:12:34 +0900

    test: make cusotmer show test

---
+++ src/tests/Feature/Customer/ShowTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Tests\Feature\Customer;
+
+use App\Models\Customer;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ShowTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * A basic feature test example.
+     */
+    public function test_getCustomerDetail(): void
+    {
+        /** @var \Illuminate\Contracts\Auth\Authenticatable $user */
+        $user = User::factory()->create();
+        $customer = Customer::factory()->create();
+
+        // 実行
+        $response = $this->actingAs($user)->getJson('/api/customers/' . $customer->id);
+        $response
+            ->assertOK()
+            ->assertJsonFragment([
+                'id' => $customer->id,
+                'name' => $customer->name,
+                'phone' => $customer->phone,
+                'gender' => $customer->gender,
+                'birth_date' => $customer->birth_date,
+                'postal_code' => $customer->postal_code,
+                'pref' => (string) $customer->pref,
+                'city' => $customer->city,
+                'street' => $customer->street,
+                'avatar' => $customer->avatar,
+            ]);
+    }
+}
