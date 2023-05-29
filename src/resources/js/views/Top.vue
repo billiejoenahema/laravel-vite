cecd6679aaa2d397d206981b9bcb607ceda26197
billiejoenahema <billiejoenahema@gmail.com>
2023-05-30 07:27:02 +0900

    improve: text link in Top page

--- src/resources/js/views/Top.vue
+++ src/resources/js/views/Top.vue
@@ -1,13 +1,19 @@
 <script setup>
 import InputSelectPrefecture from '@/components/InputSelectPrefecture.vue';
 import { ref } from 'vue';
+import router from '../router';
+import { store } from '../store';
 const pref = ref(13);
+const moveToCustomerIndex = () => {
+  store.commit('customers/resetParams');
+  router.push('/customers');
+};
 </script>
 
 <template>
   <h2>Top Page</h2>
   <div class="mb-3">
-    <router-link to="/customers">顧客一覧</router-link>
+    <span class="text-link" @click="moveToCustomerIndex">顧客一覧</span>
   </div>
   <InputSelectPrefecture id="prefecture" v-model="pref" />
 </template>
