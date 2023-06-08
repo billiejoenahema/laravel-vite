ad990c765239e9635009d109cb2c57657673b6d6
billiejoenahema <billiejoenahema@gmail.com>
2023-06-09 07:30:08 +0900

    feat: make CustomerObserver

---
+++ src/app/Observers/CustomerObserver.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Customer;
+use Illuminate\Support\Facades\Storage;
+
+class CustomerObserver
+{
+    /**
+     * Handle the Customer "created" event.
+     */
+    public function created(Customer $customer): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Customer "updated" event.
+     */
+    public function updated(Customer $customer): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Customer "deleted" event.
+     */
+    public function deleted(Customer $customer): void
+    {
+        // アイコン画像が存在すれば削除する
+        if ($customer->avatar) {
+            Storage::disk('public')->delete(str_replace('storage/', '', $customer->avatar));
+        }
+    }
+
+    /**
+     * Handle the Customer "restored" event.
+     */
+    public function restored(Customer $customer): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Customer "force deleted" event.
+     */
+    public function forceDeleted(Customer $customer): void
+    {
+        //
+    }
+}
