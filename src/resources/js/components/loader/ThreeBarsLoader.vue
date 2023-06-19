370b593dd580e08c5f159ddbbdba800cfbeb863c
billiejoenahema <billiejoenahema@gmail.com>
2023-06-20 07:32:35 +0900

    feat: make TreeBarsLoader component

---
+++ src/resources/js/components/loader/ThreeBarsLoader.vue
@@ -0,0 +1,90 @@
+<script setup>
+import { ref } from 'vue';
+
+defineProps({
+  loading: {
+    type: Boolean,
+    default: false,
+  },
+});
+const color = ref('#fca35a');
+</script>
+
+<template>
+  <div class="overlay" v-if="loading">
+    <div class="loader"></div>
+  </div>
+</template>
+
+<style scoped>
+.overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 10000;
+}
+.loader,
+.loader:before,
+.loader:after {
+  background: v-bind(color);
+  -webkit-animation: load1 1s infinite ease-in-out;
+  animation: load1 1s infinite ease-in-out;
+  width: 1em;
+  height: 4em;
+}
+.loader {
+  color: v-bind(color);
+  text-indent: -9999em;
+  margin: 88px auto;
+  position: relative;
+  font-size: 11px;
+  -webkit-transform: translateZ(0);
+  -ms-transform: translateZ(0);
+  transform: translateZ(0);
+  -webkit-animation-delay: -0.16s;
+  animation-delay: -0.16s;
+}
+.loader:before,
+.loader:after {
+  position: absolute;
+  top: 0;
+  content: '';
+}
+.loader:before {
+  left: -1.5em;
+  -webkit-animation-delay: -0.32s;
+  animation-delay: -0.32s;
+}
+.loader:after {
+  left: 1.5em;
+}
+@-webkit-keyframes load1 {
+  0%,
+  80%,
+  100% {
+    box-shadow: 0 0;
+    height: 4em;
+  }
+  40% {
+    box-shadow: 0 -2em;
+    height: 5em;
+  }
+}
+@keyframes load1 {
+  0%,
+  80%,
+  100% {
+    box-shadow: 0 0;
+    height: 4em;
+  }
+  40% {
+    box-shadow: 0 -2em;
+    height: 5em;
+  }
+}
+</style>
