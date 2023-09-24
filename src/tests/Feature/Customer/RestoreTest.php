052d3df0b4ee4f7bbe8a6d6c65143402b6c183fe
billiejoenahema <billiejoenahema@gmail.com>
2023-09-25 07:55:02 +0900

    test: make customer restore test

---
+++ src/tests/Feature/Customer/RestoreTest.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Feature\Customer;
+
+use App\Models\Customer;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+final class RestoreTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * テスト前の共通処理
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        /** @var \Illuminate\Contracts\Auth\Authenticatable $user */
+        $this->user = User::factory()->create();
+        $this->customer = Customer::factory()->create(['avatar' => 'test.png']);
+        $this->customer->delete();
+    }
+
+    /**
+     * 一般ユーザーが削除された顧客を復元できないこと。
+     */
+    public function test_general_user_cannot_restore_customer(): void
+    {
+        $response = $this->actingAs($this->user)->patchJson('/api/customers/' . $this->customer->id . '/restore');
+        $response->assertForbidden();
+    }
+
+    /**
+     * 管理者ユーザーが削除された顧客を復元できること。
+     */
+    public function test_admin_user_can_restore_customer(): void
+    {
+        $this->user->role = User::ROLE_ADMIN;
+        $this->user->save();
+
+        $response = $this->actingAs($this->user)->patchJson('/api/customers/' . $this->customer->id . '/restore');
+        $response->assertOk();
+    }
+}
