40ac784763a1c60b2456368fa4e04676be7614c3
billiejoenahema <billiejoenahema@gmail.com>
2023-10-08 08:32:53 +0900

    feat: make notice migration&factory&seeder

---
+++ src/database/factories/NoticeFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Notice>
+ */
+class NoticeFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' =>  $this->faker->realText(10, 5),
+            'content' =>  $this->faker->realText(50, 5),
+        ];
+    }
+}
